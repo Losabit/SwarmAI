1ecc91ce199cfd574de7e6961b659a9408f9661c
Paul <paulbenabou@gmail.com>
2021-09-21 12:29:56 +0200

    Improved demo

--- AntColonyOptimization.py
+++ AntColonyOptimization.py
@@ -1,12 +1,21 @@
 from DrawerHelper import *
 
-dHelper = DrawerHelper("test", 200, 200)
 
-# run the game loop
-while True:
-    dHelper.draw_rect(dHelper.BLUE, (12, 20, 40 , 50))
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+class AntColonyOptimization:
+
+    def __init__(self):
+        self.dHelper = DrawerHelper("AntColonyOptimization Example", 200, 200)
+
+    def update(self):
+        # run the game loop
+        while True:
+            self.dHelper.draw_rect(self.dHelper.BLUE, (12, 20, 40, 50))
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+
+demo = AntColonyOptimization()
+demo.update()
